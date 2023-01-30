40682caf1cb139bee42a3fda8550f330a11164d9
kimjihyeon-angela <angela9830@naver.com>
2023-01-30 17:17:18 +0900

    별표찍기

---
+++ Day01/code02_start_print.py
@@ -0,0 +1,11 @@
+# date : 2023-01-30
+# author : kimjihyeon-angela
+# desc : 별표 찍기
+
+# ()뒤에서 커서 깜박일때 ctrl+c -> ctrl+v 누르면 복붙됨
+# Alt 누르고 화살표 위, 아래 누르면 위치가 변경됨
+print('    *')
+print('   **')
+print('  ***')
+print(' ****')
+print('*****')
